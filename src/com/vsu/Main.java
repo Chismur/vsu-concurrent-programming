c6a4e4ca9bd9b5fb2654af0f9824e37382f95330
matmalik <matvey.malik@t-systems.com>
2018-10-18 22:00:46 +0300

    Minor Fix

--- src/com/vsu/Main.java
+++ src/com/vsu/Main.java
@@ -1,28 +1,30 @@
 package com.vsu;
 
-import java.io.*;
-import java.util.Random;
-
-import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
-
 public class Main {
 
-    private static final String PATH = "./matrix.txt";
+    private static final String PATH = "./resources/matrix.txt";
     //private static final String PATH_TEST = "./matrixTest.txt";
-    private static final int rows = 10;
-    private static final int cols = 10;
+    private static final int rows = 5;
+    private static final int cols = 5;
 
     public static void main(String[] args) {
 
+
         FileReaderUtils fileReaderUtils = new FileReaderUtils(PATH);
 
-        int[][] arr = fileReaderUtils.getRandomMatrix(rows,cols);
+        //int[][] arr = fileReaderUtils.getRandomMatrix(rows,cols);
 
-        fileReaderUtils.printArray(arr);
+        //fileReaderUtils.printArray(arr);
 
-        fileReaderUtils.saveArrayToFile(arr, PATH);
+
+        //fileReaderUtils.saveArrayToFile(arr, PATH);
 
         int[][] nArr = fileReaderUtils.loadArrayFromFile(PATH);
+        fileReaderUtils.printArray(nArr);
+
+        Matrix matrix = new Matrix();
+        System.out.println(nArr[4].length);
+
 
     }
 
