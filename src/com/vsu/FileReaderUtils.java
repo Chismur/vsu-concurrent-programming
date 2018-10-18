10fee7f912981acce810ee0b71ee2c3487641845
matmalik <matvey.malik@t-systems.com>
2018-10-18 21:34:17 +0300

    Create utility FileReaderUtils class for the matrices

---
+++ src/com/vsu/FileReaderUtils.java
@@ -0,0 +1,75 @@
+package com.vsu;
+
+import java.io.*;
+import java.util.Random;
+
+public class FileReaderUtils {
+
+    private String path;
+
+    public FileReaderUtils(String path) {
+        this.path = path;
+    }
+
+    public  void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void saveArrayToFile(int[][] arr, String path) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+            bw.write(String.valueOf(arr.length));
+            bw.newLine();
+            bw.write(String.valueOf(arr[0].length));
+            bw.newLine();
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    bw.write(String.valueOf(arr[i][j]));
+                    bw.newLine();
+                }
+            }
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int[][] loadArrayFromFile(String path) {
+        int[][] arr = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            // first 2 lines = rows and columns
+            int rows = Integer.parseInt(br.readLine());
+            int cols = Integer.parseInt(br.readLine());
+            arr = new int[rows][cols];
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    arr[i][j] = Integer.parseInt(br.readLine());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+
+    public int[][] getRandomMatrix(int rows, int cols){
+        int[][] arr = new int[rows][cols];
+
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = random.nextInt(((99)+1));
+            }
+        }
+        return arr;
+    }
+}
--- src/com/vsu/Main.java
+++ src/com/vsu/Main.java
@@ -1,8 +1,33 @@
 package com.vsu;
 
+import java.io.*;
+import java.util.Random;
+
+import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
+
 public class Main {
 
+    private static final String PATH = "./matrix.txt";
+    //private static final String PATH_TEST = "./matrixTest.txt";
+    private static final int rows = 10;
+    private static final int cols = 10;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        FileReaderUtils fileReaderUtils = new FileReaderUtils(PATH);
+
+        int[][] arr = fileReaderUtils.getRandomMatrix(rows,cols);
+
+        fileReaderUtils.printArray(arr);
+
+        fileReaderUtils.saveArrayToFile(arr, PATH);
+
+        int[][] nArr = fileReaderUtils.loadArrayFromFile(PATH);
+
     }
+
+
+
+
+
 }
